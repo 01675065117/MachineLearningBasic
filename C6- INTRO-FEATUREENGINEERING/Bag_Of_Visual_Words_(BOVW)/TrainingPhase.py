1a83fdf9324dfdde07a9f4a0f1c0fe41a8db267d
Trần Minh Khoa <67041926+01675065117@users.noreply.github.com>
2022-01-26 22:47:18 +0700

    update

--- C6- INTRO-FEATUREENGINEERING/Bag_Of_Visual_Words_(BOVW)/TrainingPhase.py
+++ C6- INTRO-FEATUREENGINEERING/Bag_Of_Visual_Words_(BOVW)/TrainingPhase.py
@@ -28,6 +28,50 @@ for training_name in training_names:
     class_path = imgList(dir)
     image_paths += class_path
     image_classes += [class_id]*len(class_path)
+    class_id += 1
+    
+'''
+- Create feature extraction and keypoint detector objects
+- Decription of feature extraction, keypoint detector and BRISK https://codelungtung.wordpress.com/2018/09/28/keypoint-detector-local-features-and-fast/
+- Create list where all the descriptors will be stored
+'''
+des_list = []
+
+brisk = cv2.BRISK_create(30)
+
+for image_path in image_paths:
+    im = cv2.imread(image_path)
+    keypoints, des = brisk.detectAndCompute(im, None)
+    des_list.append((image_path,des))
+    
+# Stack all the descriptors vertically in a numpy array
+descriptors = des_list[0][1]
+for image_path, descriptor in des_list[1:]:
+    descriptors = np.vstack((descriptors,descriptor))
+    
+# Kmeans works only on float, so convert integers to float
+descriptors_float = descriptors.astype(float)
+
+# Perform kmeans clustering and vector quantization
+from scipy.cluster.vq import kmeans, vq
+k = 200
+voc, variance = kmeans(descriptors_float,k,1)
+
+# Calculate the histogram of features and represent them as vector
+# vq assigns codes from a code book to observations.
+im_features = np.zeros((len(image_paths), k), "float32")
+for i in range(len(image_paths)):
+    words, distance = vq(des_list[i][1],voc)
+    for w in words:
+        im_features[i][w] += 1
+        
+# Perform Tf-Idf vectorization
+nbr_occurences = np.sum((im_features > 0) * 1, axis = 0)
+idf = np.array(np.log((1.0*len(image_paths)+1) / (1.0*nbr_occurences)), "float32")
+
+
+
+
 
 
 
